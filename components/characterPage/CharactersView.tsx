abf901b95db1b35f4225d725786ae37a4d345c52
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-05-15 00:29:24 +0200

    refactor: add CharactersView

---
+++ components/characterPage/CharactersView.tsx
@@ -0,0 +1,31 @@
+import { useQuery } from "react-query";
+import { CharacterItem } from "../CharacterItem";
+import { ICharacter } from "../../interfaces/interfaces";
+import { Services } from "../../services/services";
+
+export const CharactersView = () => {
+  const { isLoading, error, data } = useQuery(
+    "getCharacters",
+    Services.getCharacters
+  );
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>An error has occurred.</div>;
+  }
+
+  return (
+    <div className="bg-gray-50 flex flex-col">
+      characters
+      <span>{data.info.pages || ""}</span>
+      <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-5 gap-4 mx-auto">
+        {data &&
+          data.results.map((character: ICharacter) => {
+            return <CharacterItem key={character.id} data={character} />;
+          })}
+      </div>
+    </div>
+  );
+};
--- pages/characters/index.tsx
+++ pages/characters/index.tsx
@@ -1,33 +1,7 @@
-import { useQuery } from "react-query";
-import { CharacterItem } from "../../components/CharacterItem";
-import { ICharacter } from "../../interfaces/interfaces";
-import { Services } from "../../services/services";
+import { CharactersView } from "../../components/characterPage/CharactersView";
 
 const CharactersPage = () => {
-  const { isLoading, error, data } = useQuery(
-    "getCharacters",
-    Services.getCharacters
-  );
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-  if (error) {
-    return <div>An error has occurred.</div>;
-  }
-
-  return (
-    <div className="bg-gray-50 flex flex-col">
-      characters
-      <span>{data.info.pages || ""}</span>
-      <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-5 gap-4 mx-auto">
-        {data &&
-          data.results.map((character: ICharacter) => {
-            return <CharacterItem key={character.id} data={character} />;
-          })}
-      </div>
-    </div>
-  );
+  return <CharactersView />;
 };
 
 export default CharactersPage;
