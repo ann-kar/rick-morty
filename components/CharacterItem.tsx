0e532bbde61a40082d4ab63a2bc01cd0ab4bf187
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-05-15 00:27:45 +0200

    feat: display characters

---
+++ components/CharacterItem.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { ICharacter } from "../interfaces/interfaces";
+import Image from "next/image";
+
+interface CharacterItemProps {
+  data: ICharacter;
+}
+
+export const CharacterItem = ({ data }: CharacterItemProps) => {
+  const [isClicked, setIsClicked] = useState<boolean>(false);
+
+  const handleClick = () => {
+    setIsClicked(!isClicked);
+  };
+
+  return (
+    <div className={"rounded-md w-50 h-50"}>
+      <div className="font-bold py-2">{data.name}</div>
+      <Image
+        src={data.image}
+        alt={"avatar: " + data.name}
+        width="150px"
+        height="150px"
+        className="rounded-full mx-auto"
+      />
+      <button
+        className="rounded-xl shadow-md bg-gray-100 text-gray-900 text-sm p-2 px-6 m-1"
+        id={data.name}
+        onClick={handleClick}>
+        {isClicked ? "remove" : "add"}
+      </button>
+    </div>
+  );
+};
--- pages/characters/index.tsx
+++ pages/characters/index.tsx
@@ -1,8 +1,13 @@
 import { useQuery } from "react-query";
+import { CharacterItem } from "../../components/CharacterItem";
+import { ICharacter } from "../../interfaces/interfaces";
 import { Services } from "../../services/services";
 
 const CharactersPage = () => {
-  const { isLoading, error, data } = useQuery("getCharacters", Services.getCharacters);
+  const { isLoading, error, data } = useQuery(
+    "getCharacters",
+    Services.getCharacters
+  );
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -14,7 +19,13 @@ const CharactersPage = () => {
   return (
     <div className="bg-gray-50 flex flex-col">
       characters
-      <span>{data?.info?.pages || ""}</span>
+      <span>{data.info.pages || ""}</span>
+      <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-5 gap-4 mx-auto">
+        {data &&
+          data.results.map((character: ICharacter) => {
+            return <CharacterItem key={character.id} data={character} />;
+          })}
+      </div>
     </div>
   );
 };
