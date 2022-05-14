a1ed5c4d9c4fa6c5dc3d2ce6e78aefa37ff2d73b
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-05-15 00:39:55 +0200

    feat: add page

---
+++ pages/favourite-characters.tsx
@@ -0,0 +1,11 @@
+const FavouriteCharactersPage = () => {
+  return (
+    <div className="bg-gray-50 flex flex-col">
+      <h1 className="block w-full text-left mb-4 text-3xl font-medium text-gray-900">
+        Favourite characters
+      </h1>
+    </div>
+  );
+};
+
+export default FavouriteCharactersPage;
