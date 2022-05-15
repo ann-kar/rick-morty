d05545131d82cb6add9b14b1e8feab8e5e89d68c
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-05-15 23:21:19 +0200

    fix: avoid unnecessary rerenders with useMemo

--- components/FavButton.tsx
+++ components/FavButton.tsx
@@ -1,33 +1,36 @@
+import React from "react";
 import { useState, useEffect } from "react";
 import { ICharacter } from "../interfaces/interfaces";
 import { useFavourites } from "../providers/FavouritesContextProvider";
 
-export const FavButton = ({data}:{data: ICharacter}) => {
-    const { addToFavourites, removeFromFavourites, favourites } = useFavourites();
-    const [isClicked, setIsClicked] = useState<boolean>(false);
+export const FavButton = ({ data }: { data: ICharacter }) => {
+  const { addToFavourites, removeFromFavourites, favourites } = useFavourites();
+  const [isClicked, setIsClicked] = useState<boolean>(false);
 
-    useEffect(() => {
-        if (favourites.find((favCharacter) => favCharacter.id === data.id)) {
-          setIsClicked(true);
-        }
-      }, [data.id, favourites]);
+  useEffect(() => {
+    if (favourites.find((favCharacter) => favCharacter.id === data.id)) {
+      setIsClicked(true);
+    }
+  }, [data.id, favourites]);
 
-      const handleClick = () => {
-        if (isClicked) {
-          removeFromFavourites(data.id);
-          setIsClicked(false);
-        } else {
-          addToFavourites(data);
-          setIsClicked(true);
-        }
-      };
-
+  return React.useMemo(() => {
+    const handleClick = () => {
+      if (isClicked) {
+        removeFromFavourites(data.id);
+        setIsClicked(false);
+      } else {
+        addToFavourites(data);
+        setIsClicked(true);
+      }
+    };
     return (
-        <button
+      <button
         className="rounded-xl shadow-md bg-gray-100 text-gray-900 text-sm p-2 px-6 m-1"
         id={data.name}
         onClick={handleClick}>
-        {isClicked ? "remove" : "add"}
+        {isClicked ? "remove" : "add"} {Math.floor(Math.random() * 200)}
       </button>
-    )
-}
+    );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data, isClicked]);
+};
