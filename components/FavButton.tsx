26a82bdaf05b86ce430c687cb727297cc8c97e4b
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-05-16 21:29:44 +0200

    styles: update colors + hover effect

--- components/FavButton.tsx
+++ components/FavButton.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { ICharacter } from "../interfaces/interfaces";
 import { useFavourites } from "../providers/FavouritesContextProvider";
+import { StarIcon } from "@heroicons/react/solid";
 
 export const FavButton = ({ data }: { data: ICharacter }) => {
   const { addToFavourites, removeFromFavourites, favourites } = useFavourites();
@@ -25,10 +26,19 @@ export const FavButton = ({ data }: { data: ICharacter }) => {
     };
     return (
       <button
-        className="rounded-xl shadow-md bg-gray-100 text-gray-900 text-sm p-2 px-6 m-1"
+        className={`w-4/5 mx-auto rounded-xl  shadow-none bg-gradient-to-br from-gray-50 to-gray-300/70 text-gray-400 text-lg p-2 px-3 m-1 hover:text-emerald-500 active:scale-90 duration-75
+      ${isClicked && "font-extrabold from-white to-emerald-600/30"}`}
         id={data.name}
         onClick={handleClick}>
-        {isClicked ? "remove" : "add"} {Math.floor(Math.random() * 200)}
+        <span
+          className={`font-semibold text-sm px-2 tracking-wide ${
+            isClicked && "text-emerald-500"
+          }`}>
+          Favourite
+        </span>
+        <StarIcon
+          className={`h-6 w-6 inline-block ${isClicked && "text-emerald-500"}`}
+        />
       </button>
     );
     // eslint-disable-next-line react-hooks/exhaustive-deps
