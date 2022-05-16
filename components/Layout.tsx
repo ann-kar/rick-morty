88a276cddafc6409dc14971b0aeaee981d4f47e8
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-05-16 18:49:48 +0200

    styles: remove padding

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -9,7 +9,7 @@ export const Layout = ({ children }: LayoutProps) => {
   return (
     <div className="flex flex-col min-h-screen w-full">
       <Navbar/>
-      <main className="grow p-4 text-center">{children}</main>
+      <main className="grow text-center">{children}</main>
     </div>
   );
 };
