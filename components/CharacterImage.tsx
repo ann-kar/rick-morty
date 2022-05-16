826a1efadb5351080ff2eb2919064937a75f125a
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-05-16 19:56:12 +0200

    styles: different img color depending on status

--- components/CharacterImage.tsx
+++ components/CharacterImage.tsx
@@ -1,17 +1,19 @@
 import Image from "next/image";
+import { Status } from "../interfaces/interfaces";
 
 interface CharacterImageProps {
   alt: string;
   src: string;
+  characterStatus: Status;
   type: "avatar" | "profile";
 }
 
-export const CharacterImage = ({ alt, src, type }: CharacterImageProps) => {
+export const CharacterImage = ({ alt, src, type, characterStatus }: CharacterImageProps) => {
   let size, classes;
   switch (type) {
     case "avatar":
-      size = "150px";
-      classes = "rounded-full mx-auto";
+      size = "144px";
+      classes = "rounded-full";
       break;
     case "profile":
       size = "250px";
@@ -20,7 +22,7 @@ export const CharacterImage = ({ alt, src, type }: CharacterImageProps) => {
   }
 
   return (
-    <div>
+    <div className="relative border-8 mx-auto border-white shadow-zinc-600 hover:shadow-2xl w-36 h-36 rounded-full  ">
       <Image
         src={src}
         alt={`${type}: ${alt}`}
@@ -28,6 +30,7 @@ export const CharacterImage = ({ alt, src, type }: CharacterImageProps) => {
         height={size}
         className={classes}
       />
+      <div className={`absolute ${characterStatus === 'Alive' && 'bg-khaki opacity-40'} ${characterStatus === 'Dead' && 'bg-gray-700 opacity-60'} ${characterStatus === 'unknown' && 'bg-powderblue opacity-30'} top-0 left-0 w-full h-full rounded-full hover:opacity-0 duration-100`}></div>
     </div>
   );
 };
