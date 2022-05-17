e59873c7f595a76a9df38cbf1cc1d39e14aa27c3
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-05-17 23:41:50 +0200

    styles: adjust text size to screen

--- components/heroSection/HeroHeader.tsx
+++ components/heroSection/HeroHeader.tsx
@@ -5,7 +5,7 @@ interface HeroHeaderProps {
 export const HeroHeader = ({ label }: HeroHeaderProps) => {
   return (
     <>
-      <div className="text-2xl sm:text-4xl font-extrabold py-6 text-center sm:text-left text-powderblue">
+      <div className="text-2xl sm:text-4xl lg:text-6xl font-extrabold py-6 text-center sm:text-left text-powderblue">
         {label}
       </div>
     </>
