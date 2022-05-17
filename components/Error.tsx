d98003452d131c84820f676d788cf16f25eee8e8
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-05-17 17:26:51 +0200

    feat: add error type

--- components/Error.tsx
+++ components/Error.tsx
@@ -1,9 +1,22 @@
 import Image from "next/image";
 
-export const Error = () => (
+interface ErrorProps {
+  type?: "no-results";
+}
+export const Error = ({ type }: ErrorProps) => (
   <div className="flex flex-col items-center">
-
-    <Image src="/error-rick.png" width="230px" height="200px" alt="annoyed Rick" />
-    <h3>I haven&apos;t heard of such a being. And I know everything, mind you.</h3>
+    <Image
+      src="/error-rick.png"
+      width="230px"
+      height="200px"
+      alt="annoyed Rick"
+    />
+    {type === "no-results" ? (
+      <h3>
+        I haven&apos;t heard of such a being. And I know everything, mind you.
+      </h3>
+    ) : (
+      <h3>Something went wrong.</h3>
+    )}
   </div>
 );
