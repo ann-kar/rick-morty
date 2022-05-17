07b1a79ce35e130be4d83e9919f7147921a547bd
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-05-17 23:26:27 +0200

    feat: add a msg for users with no favourites

--- pages/favourite-characters.tsx
+++ pages/favourite-characters.tsx
@@ -9,20 +9,26 @@ const FavouriteCharactersPage = () => {
 
   return (
     <div className="flex flex-col">
-      <Hero header={"Your favourites!"} subheader={""}>
+      <Hero header={"Your favourite characters"} subheader={""}>
         <HeroImage
           src="/favourites.png"
           alt="main graphic: Morty and Rick happy"
         />
       </Hero>
-      <div className="grid xl:max-w-7xl p-4 grid-cols-2 grid-flow-row xs:grid-cols-3 sm:grid-cols-4 md:grid-cols-5 gap-4 mx-auto">
-        {favourites &&
-          favourites.map((character: ICharacter) => {
+      {favourites.length ? (
+        <div className="grid xl:max-w-7xl p-4 grid-cols-2 grid-flow-row xs:grid-cols-3 sm:grid-cols-4 md:grid-cols-5 gap-4 mx-auto">
+          {favourites.map((character: ICharacter) => {
             return (
               <CharacterItem key={`fav-${character.id}`} data={character} />
             );
           })}
-      </div>
+        </div>
+      ) : (
+        <div className="p-10 font-semibold text-sm text-center text-zinc-600">
+          It seems that you don&apos;t like anyone. But you can always change
+          that!
+        </div>
+      )}
     </div>
   );
 };
