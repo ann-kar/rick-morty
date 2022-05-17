0cb521a18e9e99f845b372dce1a7f1d5475eeefc
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-05-17 23:05:52 +0200

    styles: subtler styles

--- components/characterPage/SectionItem.tsx
+++ components/characterPage/SectionItem.tsx
@@ -7,7 +7,7 @@ interface SectionItemProps {
 
 export const SectionItem = ({ label, info }: SectionItemProps) => {
   return (
-    <div className="w-full rounded-md border-powderblue/10 border-2 bg-gradient-to-br from-white to-powderblue/10 text-xl p-2">
+    <div className="w-full rounded-md border-powderblue/10 border-2 bg-gradient-to-br from-white to-powderblue/10 text-md p-2">
       <InputLabel label={label} />
       <div className="ml-1 tracking-wider">{info}</div>
     </div>
