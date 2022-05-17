3f705309904e92dfb1456034230df42852e5fe44
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-05-17 23:32:05 +0200

    styles: larger font on larger screens

--- components/heroSection/HeroSubheader.tsx
+++ components/heroSection/HeroSubheader.tsx
@@ -5,7 +5,7 @@ interface HeroSubheaderProps {
 export const HeroSubheader = ({ label }: HeroSubheaderProps) => {
   return (
     <>
-      <div className="text-gray-400 pb-6 text-xs leading-4 sm:leading-5 sm:text-md xl:text-lg text-center sm:text-left">
+      <div className="text-gray-400 pb-6 text-xs leading-4 sm:leading-6 sm:text-base xl:text-lg text-center sm:text-left">
         {label}
       </div>
     </>
