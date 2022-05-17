c141d6871f8d1ca6ba507d96ec8d67abba60738a
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-05-17 15:13:35 +0200

    styles: update Hero styles & grid layout

--- pages/favourite-characters.tsx
+++ pages/favourite-characters.tsx
@@ -1,19 +1,27 @@
-import { CharacterItem } from "../components/characterPage/CharacterItem";
+import { CharacterItem } from "../components/charactersPage/CharacterItem";
 import { ICharacter } from "../interfaces/interfaces";
 import { useFavourites } from "../providers/FavouritesContextProvider";
+import { Hero } from "../components/Hero";
+import { HeroImage } from "../components/heroSection/HeroImage";
 
 const FavouriteCharactersPage = () => {
   const { favourites } = useFavourites();
 
   return (
-    <div className="bg-gray-50 flex flex-col">
-      <h1 className="block w-full text-left mb-4 text-3xl font-medium text-gray-900">
-        Favourite characters
-      </h1>
-      <div className="grid grid-cols-5">
-        {favourites && favourites.map((character: ICharacter) => {
-          return <CharacterItem key={`fav-${character.id}`} data={character} />;
-        })}
+    <div className="flex flex-col">
+      <Hero shortLabel={"the best of"} longLabel={"your favourites from"}>
+        <HeroImage
+          src="/favourites.png"
+          alt="main graphic: Morty and Rick happy"
+        />
+      </Hero>
+      <div className="grid xl:max-w-7xl p-4 grid-cols-2 grid-flow-row xs:grid-cols-3 sm:grid-cols-4 md:grid-cols-5 gap-4 mx-auto">
+        {favourites &&
+          favourites.map((character: ICharacter) => {
+            return (
+              <CharacterItem key={`fav-${character.id}`} data={character} />
+            );
+          })}
       </div>
     </div>
   );
