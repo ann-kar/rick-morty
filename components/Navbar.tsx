16d8b11ff6207314f54afa102a2dde0bdace99ac
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-05-17 23:04:23 +0200

    styles: update link label

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,7 @@ export const Navbar = () => {
     <nav className="justify-end font-semibold tracking-wide bg-gray-100 flex gap-5 p-3 uppercase text-xs sm:text-sm md:text-md">
       <NavbarLink
         href={"/favourite-characters"}
-        label={"favourite characters"}
+        label={"favourites"}
       />
       <NavbarLink href={"/characters"} label={"characters"} />
     </nav>
