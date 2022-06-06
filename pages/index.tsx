eefb36fc9bf80e5886d4f3771793c1412eb06d58
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-06-06 19:10:50 +0200

    feat: add link to "./characters" as fallback for redirect

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,17 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
+import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
     <div className="bg-emerald-200">
-      hello
+      <Link href={"./characters"}>
+        <a>
+          If you are not redirected automatically, click here to access the main
+          page
+        </a>
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
